da018d0200d12150b7dc21a1a7791e89c32b92cc
cg37 <cg37@outlook.com>
2023-01-22 20:47:44 +0800

    .

--- NC61_TwoSum/nc61_tosum.c
+++ NC61_TwoSum/nc61_tosum.c
@@ -9,10 +9,36 @@
  * @return int* returnSize 返回数组行数
  */
 #include <stdio.h>
+#include <stdlib.h>
 
 int* twoSum(int* numbers, int numbersLen, int target, int* returnSize ) {
     // write code here
     int *ans;
     ans = (int *)malloc(2*sizeof(int));
-    
+
+    for(int i=0; i<numbersLen; i++) {
+        for(int j=0; j<numbersLen; j++) {
+            if(numbers[i] + numbers[j] == target) {
+                ans[0] = i;
+                ans[1] = j;
+                *returnSize = 2;
+                return ans;
+            }
+        }
+    }
+    *returnSize = 0;
+    return ans;
+}
+
+int main() {
+    int nums[] = {11,2,14,7};
+
+    int target = 9;
+    int * returnSize = (int*) malloc (sizeof(int));
+    int * res;
+    res = twoSum(nums, 4, target, returnSize);
+    for (int i = 0; i < *returnSize; i++) {
+        printf("%d ", res[i]);
+    }
+    return 0;
 }
