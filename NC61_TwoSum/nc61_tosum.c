878b0c3848fd118ac7e07b9417b20185cd581b34
cg37 <cg37@outlook.com>
2023-01-22 20:20:07 +0800

    .

---
+++ NC61_TwoSum/nc61_tosum.c
@@ -0,0 +1,18 @@
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ * 
+ * @param numbers int整型一维数组 
+ * @param numbersLen int numbers数组长度
+ * @param target int整型 
+ * @return int整型一维数组
+ * @return int* returnSize 返回数组行数
+ */
+#include <stdio.h>
+
+int* twoSum(int* numbers, int numbersLen, int target, int* returnSize ) {
+    // write code here
+    int *ans;
+    ans = (int *)malloc(2*sizeof(int));
+    
+}
