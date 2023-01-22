9e5c0277cc4553766c7c919ac118a865ba34ed6a
cg37 <cg37@outlook.com>
2023-01-22 18:29:36 +0800

    .

--- hj5_Hextodec/hj5_hextode.c
+++ hj5_Hextodec/hj5_hextode.c
@@ -1,40 +1,60 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-int system_trans(char * ch_num,int ch_len)
+#include <stdlib.h>
+
+int hextodec(char *ch_num, int ch_len)
 {
-    if(memcmp(ch_num,"0x",2)!=0 || memcmp(ch_num,"0X",2)!=0)
-    {
-        //printf("input error!");
-        return 0;
-    }    
-    int res = 0;
-    for(int i = ch_len;i > 1;i--)
-    {
-        if(ch_num[i] >= 'A' && ch_num[i] <= 'F'){
-            res += (ch_num[i]-'A'+10)*pow(16,ch_len-i);
-        }
-        else if(ch_num[i] >= 'a' && ch_num[i] <= 'f'){
-            res += (ch_num[i]-'A'+10)*pow(16,ch_len-i);
-        }
-        else if(ch_num[i] >= '0' && ch_num[i] <= '9'){
-            res += (ch_num[i]-'0')*pow(16,ch_len-i);
-        }
-        else
-        {
-            //printf("input error!");
-        }
-    }
-    return res;
+    int num = *ch_num;
+    printf("num = %s\n", num);
+    printf("len = %d\n", ch_len);
+    // printf("ch_len = %d\n",ch_len);
+    // if(memcmp(ch_num,"0x",2)!=0 || memcmp(ch_num,"0X",2)!=0)
+    // {
+    //     printf("input error!");
+
+    //     return 0;
+    // }    
+    // int res = 0;
+    // for(int i = ch_len;i > 1;i--)
+    // {
+    //     if(ch_num[i] >= 'A' && ch_num[i] <= 'F'){
+    //         res += (ch_num[i]-'A'+10)*pow(16,ch_len-i);
+    //     }
+    //     else if(ch_num[i] >= 'a' && ch_num[i] <= 'f'){
+    //         res += (ch_num[i]-'A'+10)*pow(16,ch_len-i);
+    //     }
+    //     else if(ch_num[i] >= '0' && ch_num[i] <= '9'){
+    //         res += (ch_num[i]-'0')*pow(16,ch_len-i);
+    //     }
+    //     else
+    //     {
+    //         //printf("input error!");
+    //     }
+    // }
+    // return res;
+    return 0;
 }
 
 int main(void)
 {
     char ch_num[4] = {0};
-    while(gets(ch_num))
-    {
-        int res = system_trans(ch_num,strlen(ch_num)-1);//有效数据段是n-1
-        printf("%d\n",res);
-    }  
+    memset(ch_num, 0, sizeof(ch_num));
+    gets(ch_num);
+    //hextodec(ch_num,sizeof(ch_num));
+    printf("num = %s\n", ch_num+2);
+    printf("len = %d\n", strlen(ch_num));
+    if(memcmp(ch_num, "0x", 2) != 0 && memcmp(ch_num, "0X", 2) != 0) {
+        printf("input error");
+        return 0;
+    }
+    
+    // {
+    //     int res = system_trans(ch_num,strlen(ch_num)-1);//有效数据段是n-1
+    //     printf("%d\n",res);
+    // }  
     return 0;
+    // system_trans(char *ch_num,int ch_len)
 }
+
+
